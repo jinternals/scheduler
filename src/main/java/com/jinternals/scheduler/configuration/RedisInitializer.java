2adab3c2ede556a0d4bdcd90fcb948070c8e3b48
Mradul Pandey <mradul.pandey@tesco.com>
2020-03-30 22:16:43 +0530

    Added listener to code

---
+++ src/main/java/com/jinternals/scheduler/configuration/RedisInitializer.java
@@ -0,0 +1,29 @@
+package com.jinternals.scheduler.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+import static com.jinternals.scheduler.constants.SchedulerConstants.SCHEDULED_ITEMS_GROUP_NAME;
+import static com.jinternals.scheduler.constants.SchedulerConstants.SCHEDULED_ITEMS_STREAM_NAME;
+
+@Component
+public class RedisInitilizer {
+    private StringRedisTemplate redisTemplate;
+
+    @PostConstruct
+    public void init() {
+        try {
+            redisTemplate.opsForStream().createGroup(SCHEDULED_ITEMS_STREAM_NAME, SCHEDULED_ITEMS_GROUP_NAME);
+        }catch (Exception exception){
+            exception.printStackTrace();
+        }
+    }
+
+    @Autowired
+    public SchedulerStreamListener(StringRedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+}
